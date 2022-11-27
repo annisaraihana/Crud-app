622d32211eb4964106370c91c4ec0f09dc35e369
Ilham Faizal Hamka <ilham.faizal.hamka@mail.ugm.ac.id>
2022-11-27 13:01:35 +0700

    create error handling

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useContext, useRef } from "react";
+import { useContext, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../../context/Context";
 import NavBar from "../../components/navbar/NavBar";
@@ -9,6 +9,7 @@ import Lookist from "../../assets/lookist.png"
 export default function Login() {
   const userRef = useRef();
   const passwordRef = useRef();
+  const [error, setError] = useState("");
   const { dispatch, isFetching } = useContext(Context);
 
   const handleSubmit = async (e) => {
@@ -21,8 +22,15 @@ export default function Login() {
       });
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
       window.location = "/";
-    } catch (err) {
+    } catch (error) {
       dispatch({ type: "LOGIN_FAILURE" });
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
     }
   };
 
@@ -30,7 +38,7 @@ export default function Login() {
     <>
       <div className="BackgroundRegisterLogin object-contain bg-fixed fixed w-screen h-screen" >
         <NavBar />
-        <div className="items-start px-5 justify-center h-[480px] w-[450px] bg-white top-1/2 left-1/2 relative -translate-x-1/2 -translate-y-[290px] bg-opacity-30 rounded-xl border ">
+        <div className="items-start p-5 justify-center min-h-min w-[450px] bg-white top-1/2 left-1/2 relative -translate-x-1/2 -translate-y-[290px] bg-opacity-30 rounded-xl border ">
           <img src={Lookist} alt="" className="scale-[0.70] mx-auto" />
           <form className="flex flex-col mt-0 w-[400px] mx-auto" onSubmit={handleSubmit}>
             <label className="mt-4">Username</label>
@@ -47,7 +55,8 @@ export default function Login() {
               placeholder="Enter your password..."
               ref={passwordRef}
             />
-            <button className="mt-7 cursor-pointer bg-biru-tombol border text-white rounded-lg p-2 hover:bg-kuning hover:text-black" type="submit" disabled={isFetching}>
+            {error && <div className="mt-4 bg-red-400 text-white rounded-md p-2">{error}</div>}
+            <button className="mt-4 cursor-pointer bg-biru-tombol border text-white rounded-lg p-2 hover:bg-kuning hover:text-black" type="submit" disabled={isFetching}>
               Login
             </button>
             <div className="mt-4">
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -8,7 +8,7 @@ export default function Register() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
+  const [error, setError] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,15 +20,21 @@ export default function Register() {
         password,
       });
       res.data && window.location.replace("/login");
-    } catch (err) {
-      setError(true);
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
     }
   };
   return (
     <>
       <div className="BackgroundRegisterLogin object-contain bg-fixed fixed w-screen h-screen">
         <NavBar />
-        <div className="items-start px-5 justify-center h-[550px] w-[450px] bg-white top-1/2 left-1/2 relative -translate-x-1/2 -translate-y-[325px] bg-opacity-30 rounded-xl border">
+        <div className="items-start p-5 justify-center min-h-min w-[450px] bg-white top-1/2 left-1/2 relative -translate-x-1/2 -translate-y-[325px] bg-opacity-30 rounded-xl border">
           <img src={Lookist} alt="" className="scale-[0.70] mx-auto" />
           <form className="flex flex-col mt-0 w-[400px] mx-auto" onSubmit={handleSubmit}>
             <label className="mt-1">Username</label>
@@ -52,6 +58,7 @@ export default function Register() {
               placeholder="Enter your password..."
               onChange={(e) => setPassword(e.target.value)}
             />
+            {error && <div className="mt-4 bg-red-400 text-white rounded-md p-2">{error}</div>}
             <button className="mt-7 cursor-pointer bg-biru-tombol border text-white rounded-lg p-2 hover:bg-kuning hover:text-black" type="submit">
               Register
             </button>
