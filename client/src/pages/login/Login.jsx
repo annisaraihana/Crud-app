1430241523b101e4338e8cc88b382355c1752e0f
Ilham Faizal Hamka <ilham.faizal.hamka@mail.ugm.ac.id>
2022-11-27 10:48:34 +0700

    fixing login page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -20,6 +20,7 @@ export default function Login() {
         password: passwordRef.current.value,
       });
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+      window.location = "/";
     } catch (err) {
       dispatch({ type: "LOGIN_FAILURE" });
     }
