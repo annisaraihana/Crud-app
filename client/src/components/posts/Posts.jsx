1b745e28ec9f71070ab37338ee3aa7f6a5abd2dd
Ilham Faizal Hamka <ilham.faizal.hamka@mail.ugm.ac.id>
2022-12-06 17:09:04 +0700

    change map syntax with ternary

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -3,8 +3,8 @@ import Post from "../post/Post";
 export default function Posts({ posts }) {
   return (
     <div className="posts flex flex-wrap m-5">
-      {Object.values(posts).map((p) => (
-        <Post post={posts[p]} />
+      {posts.title?.map((p) => (
+        <Post post={p} />
       ))}
     </div>
   );
